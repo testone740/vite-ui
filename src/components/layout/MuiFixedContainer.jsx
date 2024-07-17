2220e8a5934cd0f6430bbcd547be65f392af19fb
Test One <test@test.com>
2024-07-17 17:34:23 -0400

    Basic layouts

---
+++ src/components/layout/MuiFixedContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import MuiGridLayout from './MuiGridLayout';
+
+const MuiFixedContainer = () => {
+  return (
+    <>
+      <CssBaseline />
+      <Container fixed>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+          <MuiGridLayout />
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default MuiFixedContainer;
