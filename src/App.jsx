98f1d44f1e08c7168ae0cf58d58627d29d406232
Test One <test@test.com>
2024-07-17 17:59:28 -0400

    Refactor applyCellChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MuiBasicContainer from './components/layout/MuiBasicContainer';
+import MuiFixedContainer from './components/layout/MuiFixedContainer';
+import MuiGridLayout from './components/layout/MuiGridLayout';
 
 const App = () => {
   return (
     <>
-      <MuiBasicContainer />
+      <MuiGridLayout />
     </>
   );
 };
